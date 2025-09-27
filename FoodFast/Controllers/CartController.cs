389527ea75f4fdbd77dba3b4d02dc587319c5487
xuanthuong2504 <nguyenxuanthuong254@gmail.com>
2025-09-27 20:44:01 +0700

    Tang/Giam/Xoa sp trong cart

--- FoodFast/Controllers/CartController.cs
+++ FoodFast/Controllers/CartController.cs
@@ -63,6 +63,44 @@ namespace FoodFast.Controllers
             TempData["success"] = "Decrease Product from cart successfully!";
             return RedirectToAction("Index");
         }
+        public IActionResult Increase(long Id)
+        {
+            List<CartItemModel> cart = HttpContext.Session.GetJson<List<CartItemModel>>("Cart")
+                                        ?? new List<CartItemModel>();
+
+            cart = _cartBLL.IncreaseProduct(cart, Id);
+
+            if (cart.Count == 0)
+            {
+                HttpContext.Session.Remove("Cart");
+            }
+            else
+            {
+                HttpContext.Session.SetJson("Cart", cart);
+            }
+
+            TempData["success"] = "Increase Product from cart successfully!";
+            return RedirectToAction("Index");
+        }
+        public IActionResult Remove(long Id)
+        {
+            var cart = HttpContext.Session.GetJson<List<CartItemModel>>("Cart");
+            if (cart == null) return RedirectToAction("Index");  // Tránh lỗi nếu giỏ hàng rỗng
+
+
+            cart.RemoveAll(p => p.ProductId == Id);  // Xóa sản phẩm khỏi giỏ hàng
+
+            if (cart.Count > 0)
+            {
+                HttpContext.Session.SetJson("Cart", cart);  // Cập nhật lại giỏ hàng nếu còn sản phẩm
+            }
+            else
+            {
+                HttpContext.Session.Remove("Cart");  // Xóa giỏ hàng khỏi Session nếu trống
+            }
+
+            return RedirectToAction("Index");
+        }
     }
     }
     
